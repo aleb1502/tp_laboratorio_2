22eade44d012edc14ab63982d01ce57073bc4c71
aleb1502 <31104436+aleb1502@users.noreply.github.com>
2017-09-11 20:04:53 -0300

    Add files via upload

---
+++ TP1Calculadora/TP1Calculadora/Calculadora.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1Calculadora
+{
+    public class Calculadora
+    {
+        public static double operar(Numero numero1, Numero numero2, string operador)
+        {
+            double number1 = numero1.getNumero();
+            double number2 = numero2.getNumero();
+            double result = 0;
+
+            //Buscar como castear el operador directamente.
+            switch (operador)
+            {
+                case "*":
+                    result = number1 * number2;
+                    break;
+
+                case "-":
+                    result = number1 - number2;
+                    break;
+
+                case "+":
+                    result = number1 + number2;
+                    break;
+
+                case "/":
+                    if (number2 == 0)
+                        result = 0;
+                    else
+                        result = number1 / number2;
+                    break;
+            }
+
+            return result;
+
+        }
+
+        public static string validarOperador(string operador)
+        {
+            string operatorC = "+";
+
+            if (operador == "-" || operador == "*" || operador == "/")
+            {
+                operatorC = operador;
+            }
+
+            return operatorC;
+        }
+    }
+}
---
+++ TP1Calculadora/TP1Calculadora/Numero.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TP1Calculadora
+{
+    public class Numero
+    {
+        private double numero;
+
+        public double getNumero()
+        {
+            return this.numero;
+        }
+
+        private void setNumero(string numero)
+        {
+            this.numero = Numero.validarNumero(numero);
+
+        }
+
+        private static double validarNumero(string numeroString)
+        {
+            double auxNumero;
+
+            if (double.TryParse(numeroString, out auxNumero))
+                return auxNumero;
+            else
+                return 0;
+        }
+
+
+        //public void setNumero()
+
+        public Numero()
+        {
+            this.numero = 0;
+        }
+
+        public Numero(string numero)
+        {
+            this.setNumero(numero);
+        }
+
+        public Numero(double numero1)
+        {
+            this.numero = numero1;
+        }
+    }
+}
