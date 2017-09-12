a6af804b9990d074c203cd0daf4158b81f166af0
aleb1502 <31104436+aleb1502@users.noreply.github.com>
2017-09-12 01:16:17 -0300

    Add files via upload

---
+++ TP1CalculadoraMejorado/TP1Calculadora/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP1Calculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            double auxResultado;
+            
+            Numero number1 = new Numero(txtNumero1.Text);
+            Numero number2 = new Numero(txtNumero2.Text);
+            
+            auxResultado = Calculadora.operar(number1, number2, Calculadora.validarOperador(cmbOperacion.Text)); //Si no se selecciona operador, el metodo validarOperador devuelve un + y realiza la operación con dicho operador.
+            cmbOperacion.Text = Calculadora.validarOperador(cmbOperacion.Text); //Coloca en el texto del cmb el operador actual que se está utilizando.
+            lblResultado.Text = auxResultado.ToString();
+        }
+
+        private void lblResultado_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = "0";
+            txtNumero1.Text = "";
+            txtNumero2.Text = "";
+            cmbOperacion.Text = "";
+        }
+    }
+}
---
+++ TP1CalculadoraMejorado/TP1Calculadora/Numero.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TP1Calculadora
+{
+    public class Numero
+    {
+        private double numero;
+        /// <summary> Obtiene el numero
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public double getNumero()
+        {
+            return this.numero;
+        }
+        /// <summary> Valida y crea el numero
+        /// 
+        /// </summary>
+        /// <param name="numero"></param>
+        private void setNumero(string numero)
+        {
+            this.numero = Numero.validarNumero(numero);
+
+        }
+        /// <summary> Sirve para validar el numero
+        /// 
+        /// </summary>
+        /// <param name="numeroString"></param>
+        /// <returns></returns>
+        private static double validarNumero(string numeroString)
+        {
+            double auxNumero;
+
+            if (double.TryParse(numeroString, out auxNumero))
+                return auxNumero;
+            else
+                return 0;
+        }
+
+
+        //public void setNumero()
+        /// <summary> Constructor por defecto
+        /// 
+        /// </summary>
+        public Numero()
+        {
+            this.numero = 0;
+        }
+        /// <summary> Otro constructor que recibe un string como parametro
+        /// 
+        /// </summary>
+        /// <param name="numero"></param>
+        public Numero(string numero)
+        {
+            this.setNumero(numero);
+        }
+        /// <summary> Otro constructor que recibe un double como parametro
+        /// 
+        /// </summary>
+        /// <param name="numero1"></param>
+        public Numero(double numero1)
+        {
+            this.numero = numero1;
+        }
+    }
+}
