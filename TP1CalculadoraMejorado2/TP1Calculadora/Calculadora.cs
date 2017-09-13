55fbebaa8b5f0687ec98969380810110bd420255
aleb1502 <31104436+aleb1502@users.noreply.github.com>
2017-09-13 20:44:08 -0300

    Add files via upload

---
+++ TP1CalculadoraMejorado2/TP1Calculadora/Calculadora.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1Calculadora
+{
+    public class Calculadora
+    {   /// <summary>
+    /// 
+    /// </summary> Boton que sirve para operar la calculadora
+    /// <param name="numero1"></param>
+    /// <param name="numero2"></param>
+    /// <param name="operador"></param>
+    /// <returns></returns> Retorna el resultado
+        public static double operar(Numero numero1, Numero numero2, string operador)
+        {
+            double number1 = numero1.getNumero();
+            double number2 = numero2.getNumero();
+            double result = 0;
+
+            //Buscar como castear el operador directamente.
+            switch (operador)
+            {
+                case "*":
+                    result = number1 * number2;
+                    break;
+
+                case "-":
+                    result = number1 - number2;
+                    break;
+
+                case "+":
+                    result = number1 + number2;
+                    break;
+
+                case "/":
+                    if (number2 == 0)
+                        result = 0;
+                    else
+                        result = number1 / number2;
+                    break;
+            }
+
+            return result;
+
+        }
+        /// <summary> Valida la funcion que sea una de las cuatro operaciones
+        /// 
+        /// </summary>
+        /// <param name="operador"></param>
+        /// <returns></returns> Retorna el operador validado si cumple con las condiciones de validar el operador
+        public static string validarOperador(string operador)
+        {
+            string operatorC = "+";
+
+            if (operador == "-" || operador == "*" || operador == "/")
+            {
+                operatorC = operador;
+            }
+
+            return operatorC;
+        }
+    }
+}
